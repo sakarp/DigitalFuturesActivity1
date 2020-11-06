1d79eb50ab5bd1d131c20371710f3571e3529dbe
Sakar Pudasaini <sakar@rocketmail.com>
2020-11-06 22:47:13 +0545

    next and pervious buttons working

--- server.js
+++ server.js
@@ -18,14 +18,16 @@ a1.AgeGroups.push("19-24");
 a1.AgeGroups.push("24+");
 
 a1.Countries = [];
-a1.Countries.push({ "Country": "Nepal", "Region": "South Asia" });
-a1.Countries.push({ "Country": "India", "Region": "South Asia" });
+a1.Countries.push({ Country: "Nepal", Region: "South Asia" });
+a1.Countries.push({ Country: "India", Region: "South Asia" });
 
 //set up image stack
-imgStack.stackIntro = "This beautiful photo was used on the London Metro to promote the Visit Nepal 2020 campaign. But it ran into a lot of controversy.";
+imgStack.stackIntro =
+  "This beautiful photo was used on the London Metro to promote the Visit Nepal 2020 campaign. But it ran into a lot of controversy.";
 imgStack.stackImg1 = "images/thaiBuddha.png";
 imgStack.stackImg2 = "images/thaiBuddha.png";
-imgStack.stackExplain = "<a href=\"https://kathmandupost.com/national/2019/07/08/in-embarrassing-gaffe-tourism-campaign-for-nepal-promotes-a-picture-from-thailand-in-london\" target=\"_blank\"> The Kathmandu Post </a> reports that Visit Nepal campaign used a photo of Thailand instead of Nepal for their promotion. This image was chosen from Shutterstock, a stock photo service, and was not properly verified by the designers nor the authorities."
+imgStack.stackExplain =
+  '<a href="https://kathmandupost.com/national/2019/07/08/in-embarrassing-gaffe-tourism-campaign-for-nepal-promotes-a-picture-from-thailand-in-london" target="_blank"> The Kathmandu Post </a> reports that Visit Nepal campaign used a photo of Thailand instead of Nepal for their promotion. This image was chosen from Shutterstock, a stock photo service, and was not properly verified by the designers nor the authorities.';
 
 stackArray.push(imgStack);
 
@@ -80,7 +82,7 @@ a1.Question1 = question;
 sCo.reflectHeader = "REFLECT";
 sCo.reflectQuestion1 = "What are the social consequences of this image?";
 sCo.reflectQuestion2 = "How serious are the social conseqeuences?";
-sCo.mild = "mild"
+sCo.mild = "mild";
 sCo.severe = "severe";
 
 a1.SocialConsequences = sCo;
@@ -120,12 +122,11 @@ qr = {};
 question.responseStack = responseArray;
 a1.Question2 = question;
 
-
 //-------------------------
 
-const { json } = require('body-parser');
-const express = require('express');
-const { Server } = require('http');
+const { json } = require("body-parser");
+const express = require("express");
+const { Server } = require("http");
 //const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/myTest');
@@ -140,41 +141,107 @@ const { Server } = require('http');
 //});
 
 let app = express();
-const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening on port 3000");
 });
 
-app.get("/", (req, res)=>{
-    //console.log(req);
-    res.send("Welcome to the Digital Futures Initiative");
+app.get("/", (req, res) => {
+  //console.log(req);
+  res.send("Welcome to the Digital Futures Initiative");
 });
 
-app.get("/activity1", (req, res)=>{
-    let page = req.query.page;
-    let currentStack = 0;
-    if(page){
-        a1.currentPage = page;
-        a1.currentStack = currentStack; 
-        res.render("imagestack.pug", {a1});
+app.get("/activity1", (req, res) => {
+  let page = req.query.page;
+  let currentStack = req.query.currentStack;
+  let rStr = req.query.rStr;
+
+  //if page is undefined. it means its the first call
+  //so set currentPage and currentStack
+  if (!page) {
+    a1.currentPage = 1;
+    a1.currentStack = 0;
+    a1.renderString = "imgstack.pug";
+    a1.rStr = 1;
+    res.render(a1.renderString, { a1 });
+    return;
+  }
+
+  //check if we are working with the imgstack files
+  if (rStr == 1) {
+    if (page == 6) {
+      //check if its gone to page 6, which means its time to switch image stack
+      //then increase the image stack we are working with
+      //also reset page count to 1
+      currentStack++;
+      page = 1;
+      a1.currentPage = page;
+      a1.currentStack = currentStack;
+      a1.renderString = "imgstack.pug";
+      a1.rStr = rStr;
+
+      //if we are at the end of all image stacks
+      //change the page we use to show stuff
+      if (
+        currentStack == a1.ImageStack.length ||
+        currentStack > a1.ImageStack.length
+      ) {
+        a1.renderString = "finalA1.pug";
+        a1.rStr = 2;
+        a1.currentStack = currentStack - 1;
+        a1.currentPage = 1;
+      }
+    } else {
+      //if we have not gotten to six
+      //check if we are at 0
+      if (page == 0) {
+        //reduce current stack
+        currentStack--;
+        page = 5;
+        if (currentStack < 0) {
+          console.log("crazy error");
+          //send 404 file missing message
+        }
+      }
+      a1.currentPage = page;
+      a1.currentStack = currentStack;
+      a1.renderString = "imgstack.pug";
+      a1.rStr = rStr;
     }
-    else{
-        a1.currentPage = 1; 
-        let currentStack = 0;
-        res.render("imagestack.pug", {a1});
-    }
-});
+  }
 
+  //check if we are working with the finala1.pug template
+  if (rStr == 2) {
+    // check if page is 0. this means user is trying to come back to stack
+    if (page == 0) {
+      console.log("in page 0");
+      rStr = 1; //switch the file back
+      page = 5; //switch the page to the last
+      currentStack = a1.ImageStack.length - 1; //set stack to image on stack
+      a1.currentPage = page;
+      a1.currentStack = currentStack;
+      a1.renderString = "imgstack.pug";
+      a1.rStr = 1;
+    } else {
+      a1.currentPage = page;
+      a1.currentStack = currentStack;
+      a1.renderString = "finalA1.pug";
+      a1.rStr = 2;
+    }
+  }
+
+  res.render(a1.renderString, { a1 });
+});
 
 //must be the last item
 app.use((req, res, next) => {
-    res.status(404).send({
-        status: 404,
-        error: 'Not found'
-    })
-})
+  res.status(404).send({
+    status: 404,
+    error: "Not found",
+  });
+});
